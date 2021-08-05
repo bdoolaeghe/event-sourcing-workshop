9b345a79fb0a55fc2d6906dd56832a95fc7b10cd
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2021-08-05 11:56:15 +0200

    index on event table

--- src/main/sql/1-schema.sql
+++ src/main/sql/1-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE event
 (
-    event_id                SERIAL PRIMARY KEY,
-    entity_id      TEXT  NOT NULL,
+    event_id          SERIAL PRIMARY KEY,
+    entity_id         TEXT NOT NULL,
     event_sequence_id INT  NOT NULL,
     event_type        TEXT NOT NULL,
     timestamp         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
@@ -9,10 +9,12 @@ CREATE TABLE event
     CONSTRAINT entity_id_unique UNIQUE (entity_id, event_sequence_id)
 );
 
+CREATE INDEX event_entity_id ON event (entity_id);
+
 CREATE TABLE entity
 (
-    entity_id      TEXT  PRIMARY KEY,
-    timestamp         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    entity_id TEXT PRIMARY KEY,
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE SEQUENCE entity_id_seq;
